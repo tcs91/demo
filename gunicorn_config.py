1156fc2e1a0030a50ea10f78068571cd7e9b321a
tcs91 <tataprivateconsultancy@gmail.com>
2025-06-09 18:21:02 +0530

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,9 @@
-bind = "0.0.0.0:10000"
+import os
+
+port = int(os.getenv("PORT", 10000))
+bind = f"0.0.0.0:{port}"
 workers = 4
 threads = 4
-timeout = 120 
+timeout = 120
+keepalive = 5
+worker_class = "sync" 
